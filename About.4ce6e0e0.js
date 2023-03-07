8784697280a9e03561f23101fc1de9f80060787d
Keshav Rathi <51473981+KeshavRathi28@users.noreply.github.com>
2023-03-07 13:48:18 +0530

    Updates

--- About.4ce6e0e0.js
+++ About.4ce6e0e0.js
@@ -1,2 +1,2 @@
 var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire6adc;e.register("ePKOu",(function(d,l){Object.defineProperty(d.exports,"__esModule",{value:!0}),d.exports.default=void 0;var o=e("kTWNO"),t=e("3F5Kt");var r=()=>(0,t.jsxs)("div",{className:"m-3 p-3 border border-black rounded-2xl shadow-md bg-lime-50",children:[(0,t.jsx)("h1",{className:"text-2xl font-bold",children:"About Us"}),(0,t.jsx)(o.Outlet,{})]});d.exports.default=r}));
-//# sourceMappingURL=About.2ad0cde2.js.map
+//# sourceMappingURL=About.4ce6e0e0.js.map
--- Contact.b86a61af.js
+++ Contact.b86a61af.js
@@ -1,2 +1,2 @@
 var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire6adc;e.register("kv6qB",(function(d,l){Object.defineProperty(d.exports,"__esModule",{value:!0}),d.exports.default=void 0;var o=e("3F5Kt");var a=()=>(0,o.jsx)("div",{className:"m-3 p-3 border border-black rounded-2xl shadow-md bg-lime-50",children:(0,o.jsx)("h1",{className:"text-2xl font-bold",children:"Contact Us"})});d.exports.default=a}));
-//# sourceMappingURL=Contact.5bb7d68b.js.map
+//# sourceMappingURL=Contact.b86a61af.js.map
